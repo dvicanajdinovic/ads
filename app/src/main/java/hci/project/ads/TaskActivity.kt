3d5eb15fa4ae6a971be1889d12cb528589f29706
Deno234 <denis.ibisi123@gmail.com>
2025-01-24 22:05:38 +0100

    Dodane upute kako upisati rjesenja

--- app/src/main/java/hci/project/ads/TaskActivity.kt
+++ app/src/main/java/hci/project/ads/TaskActivity.kt
@@ -160,6 +160,7 @@ class TaskActivity : AppCompatActivity() {
 
     private fun setupRememberSequence() {
         binding.sequenceTextView.visibility = View.GONE
+        binding.sequenceTaskInstruction.visibility = View.GONE
         binding.sequenceShowButton.visibility = View.VISIBLE
         sequence = generateRandomSequence()
         binding.sequenceTextView.text = sequence.joinToString(" ")
@@ -167,10 +168,12 @@ class TaskActivity : AppCompatActivity() {
             binding.sequenceTextView.visibility = View.VISIBLE
             binding.sequenceShowButton.visibility = View.GONE
             binding.sequenceUserInputText.visibility = View.GONE
+            binding.sequenceTaskInstruction.visibility = View.GONE
 
             Handler(Looper.getMainLooper()).postDelayed({
                 binding.sequenceTextView.visibility = View.GONE
                 binding.sequenceUserInputText.visibility = View.VISIBLE
+                binding.sequenceTaskInstruction.visibility = View.VISIBLE
             }, sequenceDisplayTime)
         }
     }
